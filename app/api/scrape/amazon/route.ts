77da224dfdbea11d12392effb340454e9514c16c
TidbitsJS <tidbitjs@gmail.com>
2023-09-12 15:00:23 +0530

    fix: typescript issue

--- app/api/scrape/amazon/route.ts
+++ app/api/scrape/amazon/route.ts
@@ -23,7 +23,7 @@ export async function GET(request: Request) {
     "https://www.amazon.in/Apple-2022-10-9-inch-iPad-Wi-Fi/dp/B0BJMGXLYZ/ref=pd_rhf_d_dp_s_pd_sbs_rvi_sccl_2_1/258-4802973-7066910?pd_rd_w=3Grxd&content-id=amzn1.sym.f2f99b52-a5ca-432b-8bfe-0d72feb3d1ba&pf_rd_p=f2f99b52-a5ca-432b-8bfe-0d72feb3d1ba&pf_rd_r=S2M2MAZKD10MB11BA2NJ&pd_rd_wg=91jwb&pd_rd_r=6bca717a-8384-45b2-9e29-fb68afb6a46a&pd_rd_i=B0BJMGXLYZ&th=1";
 
   try {
-    const response = await axios.get(url, options);
+    const response = await axios.get(url, options as any);
     const $ = cheerio.load(response.data);
 
     // Extract product information
