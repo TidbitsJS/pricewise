37031f2775423fc6fbaebfd58edad2493d46d399
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-09-25 23:38:29 +0530

    feat: link validation

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -5,6 +5,15 @@ import { FormEvent, useState } from "react";
 
 import { scrapeAndStoreProduct } from "@/lib/actions";
 
+function isValidAmazonProductLink(link: string) {
+  // Define a regular expression pattern to match Amazon product URLs
+  const amazonProductRegex =
+    /^(https?:\/\/)?(www\.)?amazon\.(com|in|co\.uk|ca|com\.au|de|fr|es|it|nl|jp|com\.mx|com\.br|ae|sg|com\.tr|com\.sa|com\.eg|se|com\.sg|com\.my|com\.ph|cl|com\.co|com\.ar|com\.tw|com\.cn|com\.hk|com\.sg|com\.id|com\.vn|com\.th)\/[A-Za-z0-9-]+\/dp\/[A-Za-z0-9]+(\/)?/i;
+
+  // Use the test method to check if the link matches the pattern
+  return amazonProductRegex.test(link);
+}
+
 const Searchbar = () => {
   const router = useRouter();
   const pathName = usePathname();
@@ -13,17 +22,23 @@ const Searchbar = () => {
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setIsLoading(true);
 
-    const product = await scrapeAndStoreProduct(searchPrompt, pathName);
-    console.log({ product });
+    const isValidLink = isValidAmazonProductLink(searchPrompt);
+    if (!isValidLink) return alert("Please provide valid amazon link");
 
-    if (product) {
-      const parsedProduct = JSON.parse(product);
-      router.push(`/products/${parsedProduct._id}`);
+    try {
+      setIsLoading(true);
+      const product = await scrapeAndStoreProduct(searchPrompt, pathName);
+
+      if (product) {
+        const parsedProduct = JSON.parse(product);
+        router.push(`/products/${parsedProduct._id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
-
-    setIsLoading(false);
   };
 
   return (
