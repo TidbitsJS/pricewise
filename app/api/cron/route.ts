0ea593273bc879f0d83f0ce00ee4e85efdf61472
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-09-26 00:34:10 +0530

    fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -12,6 +12,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 300; // This function can run for a maximum of 30 seconds
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET(request: Request) {
